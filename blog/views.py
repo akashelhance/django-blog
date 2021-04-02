edf64726099c66f6a30c89421b0f78fbf5ee7312
akash elhance <akashelhance@gmail.com>
2021-04-02 07:28:08 +0530

    post and marketting model setup

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 
+from posts.models import Post, Author, Category
+from marketing.models import Signup
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    queryset = Post.objects.filter(featured=True)
+    latest_post = Post.objects.filter(featured=True).order_by('-timestamp')[0:2]
+
+    if request.method == 'POST':
+        email = request.POST["email"]
+        new_signup = Signup()
+        new_signup.email = email
+        new_signup.save()
+    context ={
+        "object_list": queryset,
+        "latest": latest_post
+    }
+    return render(request, 'index.html' , context)
 
 def blog(request):
     return render(request, 'blog.html')
